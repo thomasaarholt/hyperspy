d82283442461d85468911b9b258ab5d48bf8244c
Thomas Aarholt <thomasaarholt@gmail.com>
2020-08-07 15:02:44 +0200

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -8,6 +8,6 @@ def axes_iteration(s):
 def test_bench_axes_1000(benchmark):
     "Test iterating through 1000 indices"
     s = Signal1D(zeros((10, 10, 10, 1)))
-    benchmark.pedantic(axes_iteration, args = (s,), rounds=5, iterations=5
+    benchmark.pedantic(axes_iteration, args = (s,), rounds=5, iterations=5)
     
     
