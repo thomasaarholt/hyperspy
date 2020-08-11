bf08167f9dfe88d3931f13ff80d8d80f625d3a29
Thomas Aarholt <thomasaarholt@gmail.com>
2020-08-11 10:54:51 +0200

    test slow commit

--- hyperspy/api.py
+++ hyperspy/api.py
@@ -15,7 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
-
+from time import sleep
+sleep(3)
 from hyperspy.api_nogui import *
 import logging
 _logger = logging.getLogger(__name__)
