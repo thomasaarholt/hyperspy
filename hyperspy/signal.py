307c58630eee47d08f92c81c9104056093b92b29
Eric Prestat <eric.prestat@gmail.com>
2021-02-10 17:53:25 +0000

    Add to_gpu and to_cpu method to BaseSignal.

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -46,12 +46,11 @@ from hyperspy.misc.utils import iterable_not_string
 from hyperspy.external.progressbar import progressbar
 from hyperspy.exceptions import SignalDimensionError, DataDimensionError
 from hyperspy.misc import rgb_tools
-from hyperspy.misc.utils import underline, isiterable, to_numpy
+from hyperspy.misc.utils import underline, isiterable, slugify, to_numpy
 from hyperspy.misc.hist_tools import histogram
 from hyperspy.drawing.utils import animate_legend
 from hyperspy.drawing.marker import markers_metadata_dict_to_markers
 from hyperspy.misc.slicing import SpecialSlicers, FancySlicing
-from hyperspy.misc.utils import slugify
 from hyperspy.docstrings.signal import (
     ONE_AXIS_PARAMETER, MANY_AXIS_PARAMETER, OUT_ARG, NAN_FUNC, OPTIMIZE_ARG,
     RECHUNK_ARG, SHOW_PROGRESSBAR_ARG, PARALLEL_ARG, MAX_WORKERS_ARG,
@@ -68,6 +67,12 @@ from hyperspy.exceptions import VisibleDeprecationWarning
 
 
 _logger = logging.getLogger(__name__)
+
+try:
+    import cupy as cp
+    CUPY_INSTALLED = True
+except ImportError:
+    CUPY_INSTALLED = False
 
 
 class ModelManager(object):
@@ -6106,6 +6111,15 @@ class BaseSignal(FancySlicing,
                 mask.shape != self.axes_manager.signal_shape):
             raise ValueError("The shape of signal mask array must match "
                              "`signal_shape`.")
+
+    def to_gpu(self):
+        if not CUPY_INSTALLED:
+            raise BaseException('cupy is required.')
+        else:
+            self.data = cp.asarray(self.data)
+
+    def to_cpu(self):
+        self.data = to_numpy(self.data)
 
 
 ARITHMETIC_OPERATORS = (
