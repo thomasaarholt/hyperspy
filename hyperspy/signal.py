c5f35dd626cea1e617c4a6fa7cacacf713e0aeb3
Eric Prestat <eric.prestat@gmail.com>
2021-03-02 19:35:14 +0000

    Enable use of map with cupy array.

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -4701,7 +4701,8 @@ class BaseSignal(FancySlicing,
                 for ind, res in zip(
                     range(res_data.size), executor.map(func, zip(*iterators))
                 ):
-                    res = np.asarray(res)
+                    if not hasattr(res, '__array__'):
+                        res = np.asarray(res)
                     res_data.flat[ind] = res
 
                     if ragged is False:
@@ -4723,7 +4724,8 @@ class BaseSignal(FancySlicing,
             from builtins import map
 
             for ind, res in zip(range(res_data.size), map(func, zip(*iterators))):
-                res = np.asarray(res)
+                if not hasattr(res, '__array__'):
+                    res = np.asarray(res)
                 res_data.flat[ind] = res
 
                 if ragged is False:
